c9c7ac9a88d91de68ece2f3124fadc32f011efd9
Manish <mohanaimb@gmail.com>
2023-01-26 13:55:14 +0530

    chagned height and width

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import Webcam from 'react-webcam'
 const videoConstraints = {
-  width: 1280,
-  height: 720,
+  width: "100%",
+  height: "100vh",
   facingMode: { exact: 'environment' }
 }
 
@@ -16,10 +16,10 @@ const WebcamCapture = () => {
     <>
       <Webcam
         audio={false}
-        height={720}
+        height={"100vh"}
         ref={webcamRef}
         screenshotFormat='image/jpeg'
-        width={1280}
+        width={"100%"}
         videoConstraints={videoConstraints}
       />
       <button onClick={capture}>Capture photo</button>
