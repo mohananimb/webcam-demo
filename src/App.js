69e855cf455c94219bc6451801e3c48b1e147c70
Manish <mohanaimb@gmail.com>
2023-01-26 13:53:36 +0530

    camera

--- src/App.js
+++ src/App.js
@@ -1,32 +1,30 @@
-import React from "react";
-import Webcam from "react-webcam";
+import React from 'react'
+import Webcam from 'react-webcam'
 const videoConstraints = {
   width: 1280,
   height: 720,
-  facingMode: "user"
-};
+  facingMode: { exact: 'environment' }
+}
 
 const WebcamCapture = () => {
-  const webcamRef = React.useRef(null);
-  const capture = React.useCallback(
-    () => {
-      const imageSrc = webcamRef.current.getScreenshot();console.log(imageSrc);
-    },
-    [webcamRef]
-  );
+  const webcamRef = React.useRef(null)
+  const capture = React.useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot()
+    console.log(imageSrc)
+  }, [webcamRef])
   return (
     <>
       <Webcam
         audio={false}
         height={720}
         ref={webcamRef}
-        screenshotFormat="image/jpeg"
+        screenshotFormat='image/jpeg'
         width={1280}
         videoConstraints={videoConstraints}
       />
       <button onClick={capture}>Capture photo</button>
     </>
-  );
-};
+  )
+}
 
 export default WebcamCapture
